8d2da5997c7a2e370d71775440e1df06229c3b3f
crazynet999 <51279184+crazynet999@users.noreply.github.com>
2024-04-21 12:10:12 +0900

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '323a53d5-539b-472c-9550-4b09a5f6aa8f';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
